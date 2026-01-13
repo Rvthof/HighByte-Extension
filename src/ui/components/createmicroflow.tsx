cdcf58b5237c192ddb799c7571600d4cc5d3f650
Robert van 't Hof <robert.van.thof@mendix.com>
2026-01-13 15:41:58 +0000

    added rest call into sequence flow

--- src/ui/components/createmicroflow.tsx
+++ src/ui/components/createmicroflow.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { getStudioProApi, Microflows } from '@mendix/extensions-api';
+import { getStudioProApi, Microflows, Primitives } from '@mendix/extensions-api';
 import styles from '../index.module.css';
 import { CreateMicroflowProps } from '../types';
 
 const CreateMicroflow: React.FC<CreateMicroflowProps> = ({ context, pipeline, onMicroflowCreated }) => {
     const studioPro = getStudioProApi(context);
     const messageApi = studioPro.ui.messageBoxes;
+    const microflows = studioPro.app.model.microflows;
 
     const handleCreateMicroflow = async () => {
         if (!pipeline) {
@@ -31,29 +32,72 @@ const CreateMicroflow: React.FC<CreateMicroflowProps> = ({ context, pipeline, on
 
 
             // Create the microflow first (without parameters)
-            const microflow = await studioPro.app.model.microflows.addMicroflow(containerId, {
+            const microflow = await microflows.addMicroflow(containerId, {
                 name: microflowName
             });
 
             // Use the objectCollection.addMicroflowParameterObject method
             const objectCollection = microflow.objectCollection;
+            const params = [];
 
             // Try to add parameters after creation using a for loop
             for (let i = 0; i < pipeline.requiredFields.length; i++) {
                 const field = pipeline.requiredFields[i];
                 try {
-                    const paramObj = await objectCollection.addMicroflowParameterObject({
+                    // const param = await microflows.createElement("Microflows$MicroflowParameterObject") as Microflows.MicroflowParameterObject;
+                    // param.name = field.name;
+                    
+                    // param.variableType = field.type.charAt(0).toUpperCase() + field.type.slice(1) as "Binary" | "Boolean" | "DateTime" | "Decimal" | "Float" | "Integer" | "String" | "Void";
+                    // param.relativeMiddlePoint.x += 250 * i;
+                    // objectCollection.objects.push(param);
+
+                    await objectCollection.addMicroflowParameterObject({
                         name: field.name,
                         type: field.type.charAt(0).toUpperCase() + field.type.slice(1) as "Binary" | "Boolean" | "DateTime" | "Decimal" | "Float" | "Integer" | "String" | "Void"
                     });
+                    const paramObj = await objectCollection.getMicroflowParameterObject(field.name) as Microflows.MicroflowParameterObject;
                     paramObj.relativeMiddlePoint.x += 250 * i;
+                    paramObj.size = { width: 40, height: 40 };
+
                 } catch (paramError) {
                     console.warn(`Could not add parameter ${field.name}:`, paramError);
                 }
             }
 
+            // Create and add the REST Call action activity
+            const actionActivity = await microflows.createElement("Microflows$ActionActivity") as Microflows.ActionActivity;
+            const restCall = await microflows.createElement("Microflows$RestCallAction") as Microflows.RestCallAction;
+            const httpConfiguration = await microflows.createElement("Microflows$HttpConfiguration") as Microflows.HttpConfiguration;
+            const stringTemplate = await microflows.createElement("Microflows$StringTemplate") as Microflows.StringTemplate;
+            const templateArg = await microflows.createElement("Microflows$TemplateArgument") as Microflows.TemplateArgument;
+
+            templateArg.expression = "http://127.0.0.1:8885/data/doc/index.html";
+            stringTemplate.text = "{1}";
+            stringTemplate.arguments = [templateArg];
+            httpConfiguration.customLocationTemplate = stringTemplate;
+            actionActivity.action = restCall;
+            actionActivity.size = { width: 120, height: 60 };
+
+            restCall.httpConfiguration = httpConfiguration;
+            microflow.objectCollection.objects.push(actionActivity);
+
+            microflow.flows.pop(); // Remove default flow
+
+            // Add the activity into the sequence flow of the microflow
+            const seq = await microflows.createElement("Microflows$SequenceFlow") as Microflows.SequenceFlow;
+            const startEvent = microflow.objectCollection.objects[0] as Microflows.StartEvent;
+            seq.origin = startEvent.$ID;
+            seq.destination = actionActivity.$ID;
+            microflow.flows.push(seq);
+
+            const seq2 = await microflows.createElement("Microflows$SequenceFlow") as Microflows.SequenceFlow;
+            const endEvent = microflow.objectCollection.objects[1] as Microflows.EndEvent;
+            seq2.origin = actionActivity.$ID;
+            seq2.destination = endEvent.$ID;
+            microflow.flows.push(seq2);
+
             // Save the microflow
-            await studioPro.app.model.microflows.save(microflow);
+            await microflows.save(microflow);
 
             // Notify the parent component
             onMicroflowCreated?.(microflowName);
